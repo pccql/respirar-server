c8c8f561383ec6429ff1d2a9c4baf418733c54d1
Pedro Queiroz <pccql@cin.ufpe.br>
2023-04-23 13:58:12 -0300

    feat: get availible times

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Req, UseGuards, Headers } from '@nestjs/common';
+import * as moment from 'moment-timezone';
 import { AuthService } from 'src/auth/auth.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { InterestsService } from 'src/interests/interests.service';
@@ -30,7 +31,46 @@ export class DashboardController {
     const calendar = await this.authService.getGoogleCalendarService(
       accessToken,
     );
-    const response = await calendar.events.list({ calendarId: 'primary' });
-    return response.data.items;
+
+    // Get the timezone of the calendar
+    const calendarResponse = await calendar.calendars.get({
+      calendarId: 'primary',
+    });
+    const calendarTimezone = calendarResponse.data.timeZone;
+
+    const startOfDay = moment().tz(calendarTimezone).startOf('day').hour(8);
+    const endOfDay = moment().tz(calendarTimezone).startOf('day').hour(19);
+
+    // Get events for the current day
+    const response = await calendar.events.list({
+      calendarId: 'primary',
+      timeMin: startOfDay.toISOString(true),
+      timeMax: endOfDay.toISOString(true),
+      timeZone: calendarTimezone,
+    });
+
+    const events = response.data.items;
+
+    // Find available times
+    const availableTimes = [];
+    const duration = 30; // Desired duration in minutes
+
+    for (let i = 0; i < events.length - 1; i++) {
+      const eventEnd = moment(events[i].end.dateTime).tz(calendarTimezone);
+      const nextEventStart = moment(events[i + 1].start.dateTime).tz(
+        calendarTimezone,
+      );
+
+      const availableDuration = nextEventStart.diff(eventEnd, 'minutes');
+
+      if (availableDuration >= duration) {
+        availableTimes.push({
+          start: eventEnd.format(),
+          end: nextEventStart.format(),
+        });
+      }
+    }
+
+    return availableTimes;
   }
 }
